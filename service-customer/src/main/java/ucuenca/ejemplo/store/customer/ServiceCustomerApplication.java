6e353bb615d376794d7beb1601a436a74d187c0d
dearjhon93 <dear.jhon.93@gmail.com>
2021-06-26 22:57:26 -0500

    add modified service product and shopping. Also build.gradle and servicecustomerapplicacion.java with Eureka

--- service-customer/src/main/java/ucuenca/ejemplo/store/customer/ServiceCustomerApplication.java
+++ service-customer/src/main/java/ucuenca/ejemplo/store/customer/ServiceCustomerApplication.java
@@ -2,9 +2,10 @@ package ucuenca.ejemplo.store.customer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-//@EnableEurekaClient
+@EnableEurekaClient
 public class ServiceCustomerApplication {
 
 	public static void main(String[] args) {
